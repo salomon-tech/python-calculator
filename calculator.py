cf1b2bfba6d3ef4fa9f6b7fdd0ca3572947ff36a
salomon-tech <salomondev7@gmail.com>
2025-12-02 23:24:04 +0200

    give life(fonctionality to this soft

--- calculator.py
+++ calculator.py
@@ -27,12 +27,12 @@ class Calculator():
     Button(width=11, height=4, text='8', relief='flat', bg='white', command=lambda:self.show('9')).place(x=90, y=275)
     Button(width=11, height=4, text='0', relief='flat', bg='white', command=lambda:self.show('0')).place(x=90, y=350)
     Button(width=11, height=4, text='.', relief='flat', bg='white', command=lambda:self.show('.')).place(x=180, y=350)
-    Button(width=11, height=4, text='+', relief='flat', bg='white', command=lambda:self.show('+')).place(x=270, y=350)
+    Button(width=11, height=4, text='+', relief='flat', bg='white', command=lambda:self.show('+')).place(x=270, y=275)
 
     Button(width=11, height=4, text='-', relief='flat', bg='white', command=lambda:self.show('-')).place(x=270, y=200)
     Button(width=11, height=4, text='/', relief='flat', bg='white', command=lambda:self.show('/')).place(x=270, y=50)
     Button(width=11, height=4, text='*', relief='flat', bg='white', command=lambda:self.show('*')).place(x=270, y=125)
-    Button(width=11, height=4, text='=', relief='flat', bg='white', command=lambda:self.solve).place(x=270, y=350)
+    Button(width=11, height=4, text='=', relief='flat', bg='lightblue', command=lambda:self.solve).place(x=270, y=350)
     Button(width=11, height=4, text='C', relief='flat', bg='white', command=lambda:self.clear).place(x=0, y=350)
 
   def show(self, value):
