4a4b05db880184f61899c4e18beb20efee606c26
salomon-tech <salomondev7@gmail.com>
2025-12-02 22:38:13 +0200

    give life(fonctionality to this soft

--- calculator.py
+++ calculator.py
@@ -12,6 +12,22 @@ class Calculator:
     Entry(width=17, bg='white', font=('arial', 30, 'bold'), textvariable=self.equation).place(x=0, y=0)
 
     Button(width=11, height=4, text='', relief='flat', bg='white', command=lambda:self.show()).place(x=1, y=10)
+    Button(width=11, height=4, text='', relief='flat', bg='white', command=lambda:self.show()).place(x=1, y=10)
+    Button(width=11, height=4, text='', relief='flat', bg='white', command=lambda:self.show()).place(x=1, y=10)
+    Button(width=11, height=4, text='', relief='flat', bg='white', command=lambda:self.show()).place(x=1, y=10)
+    Button(width=11, height=4, text='', relief='flat', bg='white', command=lambda:self.show()).place(x=1, y=10)
+    
+  def show(self, value):
+    self.entry_value += str(value)
+    self.equation.set(self.entry_value)
+
+  def clear(self):
+    self.entry_value = ''
+    self.equation.set(self.entry_value)
+
+  def solve(self):
+    result = eval.set(self.entry_value)
+    self.equation.set(result)
 
 root = Tk()
 run = Calculator(root)
