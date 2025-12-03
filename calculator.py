4b66fd4139e3b45c43e991882ea4d3acd95368a0
salomon-tech <salomondev7@gmail.com>
2025-12-03 17:57:07 +0200

    adding trunk to make a better and beautiful code

--- calculator.py
+++ calculator.py
@@ -3,38 +3,38 @@ from tkinter import Tk, Entry, Button, StringVar
 class Calculator():
   def __init__(self, root):
     root.title("Calculator POO")
-    root.geometry("470x600")
+    root.geometry("506x558")
     root.resizable(False, False)
     root.config(bg="gray")
 
     self.equation=StringVar()
     self.entry_value = ''
-    Entry(width=500, bg='white', font=('arial', 50, 'bold'), textvariable=self.equation).place(x=0, y=0)
+    Entry(width=500, bg='gray',bd=4, font=('arial', 50, 'bold'), textvariable=self.equation).place(x=0, y=0)
 
-    Button(width=12, height=4, text='(',bd=4, relief='flat', bg='black', fg='white', command=lambda:self.show('(')).place(x=0, y=82)
-    Button(width=12, height=4, text=')',bd=4, relief='flat', bg='black', fg='white', command=lambda:self.show(')')).place(x=123, y=82)
-    Button(width=12, height=4, text='%',bd=4, relief='flat', bg='black', fg='white', command=lambda:self.show('%')).place(x=240, y=82)
-    Button(width=12, height=4, text='/',bd=4, relief='flat', bg='black', fg='white', command=lambda:self.show('/')).place(x=363, y=82)
+    Button(width=12, height=4, text='(',bd=4,font=(20), relief='flat', bg='orange', fg='white', command=lambda:self.show('(')).place(x=0, y=82)
+    Button(width=12, height=4, text=')',bd=4,font=(20), relief='flat', bg='orange', fg='white', command=lambda:self.show(')')).place(x=123, y=82)
+    Button(width=12, height=4, text='%',bd=4,font=(20), relief='flat', bg='orange', fg='white', command=lambda:self.show('%')).place(x=240, y=82)
+    Button(width=12, height=4, text='/',bd=4,font=(20), relief='flat', bg='orange', fg='white', command=lambda:self.show('/')).place(x=363, y=82)
 
-    Button(width=12, height=4, text='1',bd=4, relief='flat', bg='black', fg='white', command=lambda:self.show('1')).place(x=0, y=172)
-    Button(width=12, height=4, text='2',bd=4, relief='flat', bg='black', fg='white', command=lambda:self.show('2')).place(x=123, y=172)
-    Button(width=12, height=4, text='3',bd=4, relief='flat', bg='black', fg='white', command=lambda:self.show('3')).place(x=240, y=172)
-    Button(width=12, height=4, text='*',bd=4, relief='flat', bg='black', fg='white', command=lambda:self.show('*')).place(x=363, y=172)
+    Button(width=12, height=4, text='1',bd=4,font=(20), relief='flat', bg='black', fg='white', command=lambda:self.show('1')).place(x=0, y=172)
+    Button(width=12, height=4, text='2',bd=4,font=(20), relief='flat', bg='black', fg='white', command=lambda:self.show('2')).place(x=123, y=172)
+    Button(width=12, height=4, text='3',bd=4,font=(20), relief='flat', bg='black', fg='white', command=lambda:self.show('3')).place(x=240, y=172)
+    Button(width=12, height=4, text='*',bd=4,font=(20), relief='flat', bg='orange', fg='white', command=lambda:self.show('*')).place(x=363, y=172)
 
-    Button(width=12, height=4, text='4',bd=4, relief='flat', bg='black', fg='white', command=lambda:self.show('4')).place(x=0, y=262)
-    Button(width=12, height=4, text='5',bd=4, relief='flat', bg='black', fg='white', command=lambda:self.show('5')).place(x=123, y=262)
-    Button(width=12, height=4, text='6',bd=4, relief='flat', bg='black', fg='white', command=lambda:self.show('6')).place(x=240, y=262)
-    Button(width=12, height=4, text='-',bd=4, relief='flat', bg='black', fg='white', command=lambda:self.show('-')).place(x=363, y=262)
+    Button(width=12, height=4, text='4',bd=4,font=(20), relief='flat', bg='black', fg='white', command=lambda:self.show('4')).place(x=0, y=262)
+    Button(width=12, height=4, text='5',bd=4,font=(20), relief='flat', bg='black', fg='white', command=lambda:self.show('5')).place(x=123, y=262)
+    Button(width=12, height=4, text='6',bd=4,font=(20), relief='flat', bg='black', fg='white', command=lambda:self.show('6')).place(x=240, y=262)
+    Button(width=12, height=4, text='-',bd=4,font=(20), relief='flat', bg='orange', fg='white', command=lambda:self.show('-')).place(x=363, y=262)
 
-    Button(width=12, height=4, text='7',bd=4, relief='flat', bg='black', fg='white', command=lambda:self.show('7')).place(x=0, y=352)
-    Button(width=12, height=4, text='8',bd=4, relief='flat', bg='black', fg='white', command=lambda:self.show('8')).place(x=123, y=352)
-    Button(width=12, height=4, text='9',bd=4, relief='flat', bg='black', fg='white', command=lambda:self.show('9')).place(x=240, y=352)
-    Button(width=12, height=4, text='+',bd=4, relief='flat', bg='black', fg='white', command=lambda:self.show('+')).place(x=363, y=352)
+    Button(width=12, height=4, text='7',bd=4,font=(20), relief='flat', bg='black', fg='white', command=lambda:self.show('7')).place(x=0, y=352)
+    Button(width=12, height=4, text='8',bd=4,font=(20), relief='flat', bg='black', fg='white', command=lambda:self.show('8')).place(x=123, y=352)
+    Button(width=12, height=4, text='9',bd=4,font=(20), relief='flat', bg='black', fg='white', command=lambda:self.show('9')).place(x=240, y=352)
+    Button(width=12, height=4, text='+',bd=4,font=(20), relief='flat', bg='orange', fg='white', command=lambda:self.show('+')).place(x=363, y=352)
 
-    Button(width=12, height=4, text='C',bd=4, relief='flat', bg='black', fg='white', command=lambda:self.clear()).place(x=0, y=442)
-    Button(width=12, height=4, text='0',bd=4, relief='flat', bg='black', fg='white', command=lambda:self.show('0')).place(x=123, y=442)
-    Button(width=12, height=4, text='.',bd=4, relief='flat', bg='black', fg='white', command=lambda:self.show('.')).place(x=240, y=442)
-    Button(width=12, height=4, text='=',bd=4, relief='flat', bg='orange', command=lambda:self.solve()).place(x=363, y=442)
+    Button(width=12, height=4, text='C',bd=4,font=(20), relief='flat', bg='black', fg='white', command=lambda:self.clear()).place(x=0, y=442)
+    Button(width=12, height=4, text='0',bd=4,font=(20), relief='flat', bg='black', fg='white', command=lambda:self.show('0')).place(x=123, y=442)
+    Button(width=12, height=4, text='.',bd=4,font=(20), relief='flat', bg='orange', fg='white', command=lambda:self.show('.')).place(x=240, y=442)
+    Button(width=12, height=4, text='=',bd=4,font=(20), relief='flat', bg='orange', command=lambda:self.solve()).place(x=363, y=442)
 
   def show(self, value):
     self.entry_value += str(value)
